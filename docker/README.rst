258358eeaefcdb66be38819672d5f28078ee3371
C. Titus Brown <titus@idyll.org>
2015-11-20 19:00:17 -0800

    updated

--- docker/README.rst
+++ docker/README.rst
@@ -1,14 +1,33 @@
-on local::
+## Create docker containers from khmer-protocols ctb branch
 
-   docker create -v /mnt --name kp-test-data ubuntu:14.04 /bin/true
+     ./extract-from-github.py https://github.com/dib-lab/khmer-protocols/tree/ctb/mrnaseq
+     cd ./output/
+     bash build.sh
 
-   docker run --rm --volumes-from kp-test-data -it ubuntu:14.04 bash
+This will create docker images 'eel-pond/1-quality:ctb', etc. ('ctb' from
+the branch, 'eel-pond' from literate-resting.json in the mrnaseq/ subdir,
+'1-quality' from the .rst filename.)
 
-on docker machine::
+## Configure data volumes
 
-   apt-get -y install curl
-   mkdir /mnt/data
-   cd /mnt/data
-   curl -O https://s3.amazonaws.com/public.ged.msu.edu/mrnaseq-subset.tar
-   tar xf mrnaseq-subset.tar
-   rm mrnaseq-subset.tar
+Load the data to be assembled:
+
+     docker create -v /mnt --name kp-test-data ubuntu:14.04 /bin/true
+     docker run --rm --volumes-from kp-test-data -it diblab/kp-base bash
+
+then on the docker machine:
+
+     mkdir /mnt/data
+     cd /mnt/data
+     curl -O https://s3.amazonaws.com/public.ged.msu.edu/mrnaseq-subset.tar
+     tar xf mrnaseq-subset.tar
+     rm mrnaseq-subset.tar
+
+## Run the three khmer-protocols docker images
+
+     docker run --volumes-from kp-test-data -it eel-pond/1-quality:ctb
+     docker run --volumes-from kp-test-data -it eel-pond/2-diginorm:ctb
+     docker run --volumes-from kp-test-data -it eel-pond/3-big-assembly:ctb
+
+Assembly will be on 'kp-test-data' data volume under
+/mnt/work/trinity_out_dir/Trinity.fasta.
--- extract-from-github.py
+++ extract-from-github.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python
+"""
+Docker foo.
+"""
+import sys
 import urlparse
 import json
 import urllib
@@ -6,9 +10,10 @@ import argparse
 import tempfile
 import subprocess
 import os
+import stat
 import glob
-import sys
-sys.path.insert('/Users/t/dev/literate-resting')
+
+from scanner import scan, scantag
 
 def parse_github_url(url):
     o = urlparse.urlparse(url)
@@ -85,29 +90,105 @@ def test_load_config_from_github_1():
     assert conf['format'] == 'reST'
 
 
+def build_dockerdir(inp_filename, outdirname):
+    print >>sys.stderr, 'building Dockerfile for {} in {}'.format(
+        os.path.basename(inp_filename), outdirname)
+    
+    fp = open(inp_filename)
+    docker_cmds = list(scantag.parse_commands(fp,
+                                              os.path.basename(inp_filename),
+                                              'docker'))
+    fp = open(inp_filename)
+    script_cmds = list(scan.parse_commands(fp,
+                                           os.path.basename(inp_filename),
+                                           show_hidden=False))
+
+    entrypoint_script = os.path.basename(inp_filename) + '.sh'
+
+    # write out the Dockerfile
+    dockerfp = open(os.path.join(outdirname, 'Dockerfile'), 'w')
+    dockerfp.write("""\
+FROM diblab/kp-base
+{}
+COPY {} /home
+CMD ["/home/{}"]
+""".format("".join(docker_cmds),
+           entrypoint_script,
+           entrypoint_script))
+    dockerfp.close()
+
+    # write out the entrypoint script for the docker machine
+    entrypoint_fullname = os.path.join(outdirname, entrypoint_script)
+    entryfp = open(entrypoint_fullname, 'w')
+    entryfp.write("#! /bin/bash\n")
+    entryfp.write("".join(script_cmds))
+    entryfp.close()
+
+    # chmod +x it
+    st = os.stat(entrypoint_fullname)
+    os.chmod(entrypoint_fullname, st.st_mode | stat.S_IEXEC)
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output-dir', type=str, default='output')
+    parser.add_argument('--local-repo', type=str, default=None,
+                        help='local copy of repo for testing & speed')
     parser.add_argument('github_url')
     args = parser.parse_args()
 
     url = args.github_url
     
-    print 'looking at github url', url
+    print 'looking at github url:'
+    print '\t', url
     org, repo, branch, location = parse_github_url(url)
 
     print 'loading config from', org, repo, branch, location
     conf = load_config_from_github(org, repo, branch, location)
 
     workdir = tempfile.mkdtemp()
+
+    if args.local_repo:
+        clone_url = args.local_repo
+    else:
+        clone_url = 'http://github.com/{}/{}.git'.format(org, repo)
     
-    clone_url = 'http://github.com/{}/{}.git'.format(org, repo)
     subprocess.call(['git', 'clone', clone_url, 'repo', '-b', branch],
                     cwd=workdir)
     fileglob = os.path.join(workdir, 'repo', location.lstrip('/'),
                             conf['filesGlob'])
-    print fileglob
+    print 'looking for', conf['filesGlob'], 'in repo'
     files = glob.glob(fileglob)
-    print files
+
+    output_path = args.output_dir
+    try:
+        os.mkdir(output_path)
+    except OSError:
+        pass
+
+    output_list = []
+    for filename in files:
+        ident = os.path.basename(filename)
+        if ident.endswith('.rst'):
+            ident = ident[:-4]
+
+        output_dir = os.path.join(output_path, 'docker-' + ident)
+        try:
+            os.mkdir(output_dir)
+        except OSError:
+            pass
+        build_dockerdir(filename, output_dir)
+        output_list.append((ident, 'docker-' + ident))
+
+    build_sh = os.path.join(output_path, 'build.sh')
+    build_fp = open(build_sh, 'w')
+    print >>build_fp, 'set -x'
+    for ident, dirname in output_list:
+        tagname = '{}/{}:{}'.format(conf['prefix'], ident, branch)
+        print >>build_fp, 'docker build -t {} {}'.format(tagname, dirname)
+    build_fp.close()
+
+    print >>sys.stderr, '\nBuild commands in:', build_sh, '\n'
 
 
 if __name__ == '__main__':
